590b3e6bac5ee4f540a8754e105237b09a8f4a6f
Arthur_XC14 <arthurvfbarros@gmail.com>
2026-05-20 14:21:38 -0300

    remoção de emojis

--- src/app/home/Tipagem-pokemon.pipe.ts
+++ src/app/home/Tipagem-pokemon.pipe.ts
@@ -9,24 +9,24 @@ export class PokemonTypePipe implements PipeTransform {
   transform(type: string): string {
     if (!type) return '';
     const typeDictionary: { [key: string]: string } = {
-      normal: '⚪ Normal',
-      fire: '🔥 Fogo',
-      water: '💧 Água',
-      grass: '🌿 Planta',
-      electric: '⚡ Elétrico',
-      ice: '❄️ Gelo',
-      fighting: '🥊 Lutador',
-      poison: '☠️ Venenoso',
-      ground: '🪨 Terrestre',
-      flying: '🦅 Voador',
-      psychic: '🔮 Psíquico',
-      bug: '🐛 Inseto',
-      rock: '🪨 Pedra',
-      ghost: '👻 Fantasma',
-      dragon: '🐉 Dragão',
-      dark: '🌙 Sombrio',
-      steel: '⚙️ Aço',
-      fairy: '✨ Fada'
+      normal: 'Normal',
+      fire: 'Fogo',
+      water: 'Água',
+      grass: 'Planta',
+      electric: 'Elétrico',
+      ice: 'Gelo',
+      fighting: 'Lutador',
+      poison: 'Venenoso',
+      ground: 'Terrestre',
+      flying: 'Voador',
+      psychic: 'Psíquico',
+      bug: 'Inseto',
+      rock: 'Pedra',
+      ghost: 'Fantasma',
+      dragon: 'Dragão',
+      dark: 'Sombrio',
+      steel: 'Aço',
+      fairy: 'Fada'
     };
 
     return typeDictionary[type.toLowerCase()] || type;
