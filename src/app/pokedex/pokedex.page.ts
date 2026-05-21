375fae11fecaf974336d57cbd32caf119278b045
Arthur_XC14 <arthurvfbarros@gmail.com>
2026-05-21 10:00:18 -0300

    botão de retorno na pokedéx

--- src/app/pokedex/pokedex.page.ts
+++ src/app/pokedex/pokedex.page.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { closeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.page.html',
   styleUrls: ['./pokedex.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PokedexPage implements OnInit {
+  private readonly router = inject(Router);
 
   tentativas: number = 0;
   pokemons: any[] = []; 
 
-  constructor() { }
+  constructor() { 
+    addIcons({closeOutline});
+  }
 
   ngOnInit() {
+  }
+
+  voltar(): void{
+    this.router.navigate(['/home']);
   }
 
 }
