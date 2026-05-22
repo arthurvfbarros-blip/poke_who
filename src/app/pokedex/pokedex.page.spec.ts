60df08dd266f4e35e3d502c55bf4d38b0b412741
ravilock <ravi.me.professional@hotmail.com>
2026-05-21 21:02:55 -0300

    fix: tests

--- src/app/pokedex/pokedex.page.spec.ts
+++ src/app/pokedex/pokedex.page.spec.ts
@@ -1,11 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 import { PokedexPage } from './pokedex.page';
+import { PokemonService } from '../services/pokemon.service';
 
 describe('PokedexPage', () => {
   let component: PokedexPage;
   let fixture: ComponentFixture<PokedexPage>;
+  let pokemonServiceSpy: jasmine.SpyObj<PokemonService>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    pokemonServiceSpy = jasmine.createSpyObj('PokemonService', [
+      'obterEstatisticas',
+      'limparEstatisticas',
+    ]);
+    pokemonServiceSpy.obterEstatisticas.and.returnValue([]);
+
+    await TestBed.configureTestingModule({
+      imports: [PokedexPage],
+      providers: [
+        provideIonicAngular(),
+        provideRouter([]),
+        { provide: PokemonService, useValue: pokemonServiceSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PokedexPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/services/pokedex.spec.ts
+++ src/app/services/pokedex.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Pokedex } from './pokedex';
+import { Pokedex } from './pokedex.service';
 
 describe('Pokedex', () => {
   let service: Pokedex;
